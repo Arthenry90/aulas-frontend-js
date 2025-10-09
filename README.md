91068bea7d9bea310aaa742dd6d845e306b02380
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-08 22:45:41 -0300

    add aula 7

---
+++ README.md
@@ -0,0 +1,3 @@
+# Aulas de javascript FIAP
+
+[Link de material original ](https://github.com/mmamorim/webdev-2025-1ESPR)
---
+++ aula07/main.js
@@ -0,0 +1,11 @@
+let op = ""
+
+
+// while(op != "0"){
+//     op = prompt("Digite o número")
+// }
+
+let i = 0
+for(;i<10;i=i+2){
+    console.log("N",i)
+}
