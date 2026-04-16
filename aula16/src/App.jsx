2f45c8de4398082ae100be0d459185f70a90e505
artur <arturhs2018@gmail.com>
2026-04-15 22:52:38 -0300

    feat: add aula16

--- aula16/src/App.jsx
+++ aula16/src/App.jsx
@@ -1,10 +1,26 @@
 
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Sobre from './routes/Sobre'
+import Nav from './components/Nav'
+import Home from './routes/Home'
+import Erro from './routes/Erro'
+import Footer from './components/Footer'
+
 function App() {
 
+
   return (
-    <>
-    </>
+    <Router>
+      <Nav/>
+        <Routes>
+              <Route path="*" element={<Erro/>}/>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/sobre" element={<Sobre/>}/>
+        </Routes>
+        <Footer/>
+    </Router>
   )
+
 }
 
 export default App
